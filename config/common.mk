33902a0b067741f547a46fceb2a796f2ec5f568c
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-08 22:02:37 +0200

    vendor: BiancaProject's bootanimation based lineage
    Signed-off-by: alanndz <alanndz@qmail.id>

--- config/common.mk
+++ config/common.mk
@@ -95,6 +95,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Bootanimation
+TARGET_SCREEN_WIDTH ?= 1080
+TARGET_SCREEN_HEIGHT ?= 1920
+PRODUCT_PACKAGES += \
+    bootanimation.zip
+
 # Fonts
 include vendor/bianca/config/fonts.mk
 
