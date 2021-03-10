68bba4263d780a6f5aeeaba883d5cd8bf5312909
Kry9toN <dhimasbagusprayoga@gmail.com>
2021-03-10 11:49:40 +0000

    vendor: remove useless prop
    Change-Id: I46d9688683443b4cd63d251b68524ee1584c18fe

--- config/branding.mk
+++ config/branding.mk
@@ -28,7 +28,6 @@ ifeq ($(KOMODO_VARIANT), RELEASE)
       KOMODO_OTA_VERSION_CODE := 11
 
       KOMODO_PROPERTIES += \
-         org.komodo.ota.version_code=$(KOMODO_OTA_VERSION_CODE) \
          sys.ota.disable_uncrypt=1
 
       PRODUCT_PACKAGES += \
