28951904b0aecfbe90245196617f034c14decebf
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2021-03-10 08:49:50 +0700

    vendor: add auto version also empty url and fix syntax

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -9,20 +9,22 @@ then
     datetime=$(grep org\.komodo\.build_date_utc $OUT/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
     maintainer=$(grep org\.komodo\.maintainer $OUT/system/build.prop | cut -d= -f2)
+    version=$(grep org\.komodo\.version $OUT/system/build.prop | cut -d= -f2);
     echo "{" > $file_path.json
     echo "  \"error\"       : false," >> $file_path.json
     echo "  \"id\"          : \"$id\"," >> $file_path.json
     echo "  \"filename\"    : \"$file_name\"," >> $file_path.json
     echo "  \"datetime\"    : $datetime," >> $file_path.json
-    echo "  \"version\"     : \"2.5\"," >> $file_path.json
+    echo "  \"version\"     : \"$version\"," >> $file_path.json
     echo "  \"size\"        : $file_size," >> $file_path.json
     echo "  \"filehash\"    : \"$md5\"," >> $file_path.json
+    echo "  \"url\"         : \"\"," >> $file_path.json
     echo "  \"website_url\" : \"https://komodo-os.my.id\"," >> $file_path.json
     echo "  \"news_url\"    : \"https://t.me/KomodOSRom\"," >> $file_path.json
     echo "  \"donate_url\"  : \"https://www.paypal.me/KomodoOS\"," >> $file_path.json
     echo "  \"maintainer\"  : \"$maintainer\"," >> $file_path.json
     echo "  \"maintainer_url\" : \"https://github.com/\"," >> $file_path.json
-    echo "  \"forum_url\"      : \"https://t.me/KomodOSGroup\"," >> $file_path.json
+    echo "  \"forum_url\"      : \"https://t.me/KomodOSGroup\" " >> $file_path.json
     echo "}" >> $file_path.json
   fi
 fi
