bdcbd75b6c6400c5094dab57d5a310ee711f35de
Danny Lin <danny@kdrag0n.dev>
2022-02-19 22:41:04 -0800

    packages: Build GmsCompat companion
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -100,6 +100,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     rsync
 
+# Sandboxed Google Play
+PRODUCT_PACKAGES += \
+    GmsCompat \
+
 # These packages are excluded from user builds
 PRODUCT_PACKAGES_DEBUG += \
     procmem
