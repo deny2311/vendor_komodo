deny2311/vendor_evolution

same libbinder camera

config/BoardConfigSoong.mk
@@ -39,7 +39,7 @@ SOONG_CONFIG_evolutionGlobalVars += \
     target_process_sdk_version_override \
     target_surfaceflinger_udfps_lib \
     target_uses_prebuilt_dynamic_partitions \
-    uses_camera_parameter_lib
+    uses_camera_parameter_lib \
 
 SOONG_CONFIG_NAMESPACES += evolutionNvidiaVars
 SOONG_CONFIG_evolutionNvidiaVars += \
@@ -55,7 +55,8 @@ SOONG_CONFIG_evolutionQcomVars += \
     uses_pre_uplink_features_netmgrd \
     uses_qcom_bsp_legacy \
     uses_qti_camera_device \
-    needs_camera_boottime_timestamp
+    needs_camera_boottime_timestamp \
+    libbinder_has_the_same_header
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
@@ -81,6 +82,7 @@ SOONG_CONFIG_evolutionQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES
 SOONG_CONFIG_evolutionQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_evolutionQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 SOONG_CONFIG_evolutionQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_evolutionQcomVars_libbinder_has_the_same_header := $(TARGET_SAME_BINDER_HEADER_WITH_SYSTEM)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

deny2311/vendor_komodo

config/BoardConfigSoong.mk
@@ -39,7 +39,7 @@ SOONG_CONFIG_komodoGlobalVars += \
     target_ld_shim_libs \
     target_process_sdk_version_override \
     target_surfaceflinger_udfps_lib \
-    uses_camera_parameter_lib
+    uses_camera_parameter_lib \
 
 SOONG_CONFIG_NAMESPACES += komodoNvidiaVars
 SOONG_CONFIG_komodoNvidiaVars += \
@@ -54,7 +54,8 @@ SOONG_CONFIG_komodoQcomVars += \
     uses_pre_uplink_features_netmgrd \
     uses_qcom_bsp_legacy \
     uses_qti_camera_device \
-    needs_camera_boottime_timestamp
+    needs_camera_boottime_timestamp \
+    libbinder_has_the_same_header
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
@@ -76,6 +77,7 @@ SOONG_CONFIG_komodoQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PR
 SOONG_CONFIG_komodoQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_komodoQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 SOONG_CONFIG_komodoQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_komodoQcomVars_libbinder_has_the_same_header := $(TARGET_SAME_BINDER_HEADER_WITH_SYSTEM)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0